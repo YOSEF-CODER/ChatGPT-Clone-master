a4d73181c97692171649900b1654385a7c2af7b4
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-01-04 21:04:09 +0300

    .

--- client/script.js
+++ client/script.js
@@ -3,44 +3,46 @@ import user from "./assets/user.svg";
 
 const form = document.querySelector("form");
 const chatContainer = document.querySelector("#chat_container");
+const hero = document.querySelector('.hero')
+
 
 let loadInterval;
 
 function loader(element) {
-  element.textContent = "";
+    element.textContent = "";
 
-  loadInterval = setInterval(() => {
-    element.textContent += ".";
+    loadInterval = setInterval(() => {
+        element.textContent += ".";
 
-    if (element.textContent === "....") {
-      element.textContent = "";
-    }
-  }, 300);
+        if (element.textContent === "....") {
+            element.textContent = "";
+        }
+    }, 300);
 }
 
 function typetext(element, text) {
-  let index = 0;
+    let index = 0;
 
-  let interval = setInterval(() => {
-    if (index < text.length) {
-      element.innerHTML += text.charAt(index);
-      index++;
-    } else {
-      clearInterval(interval);
-    }
-  }, 20);
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index);
+            index++;
+        } else {
+            clearInterval(interval);
+        }
+    }, 20);
 }
 
 function generateUniqueId() {
-  const timestamp = Date.now();
-  const randomNumber = Math.random();
-  const hexadecimalString = randomNumber.toString(16);
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
 
-  return `id-${timestamp}-${hexadecimalString}`;
+    return `id-${timestamp}-${hexadecimalString}`;
 }
 
 function chatStripe(isAi, value, uniqueId) {
-  return `
+    return `
       <div class="wrapper ${isAi && "ai"}">
         <div class="chat">
           <div class="profile">
@@ -55,58 +57,59 @@ function chatStripe(isAi, value, uniqueId) {
     `;
 }
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
+const handleSubmit = async(e) => {
+    e.preventDefault();
+    hero.classList.add('deactivate')
+    const data = new FormData(form);
 
-  const data = new FormData(form);
+    //users chat stripe
+    chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
 
-  //users chat stripe
-  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+    // clear the textarea
+    form.reset();
 
-  // clear the textarea
-  form.reset();
+    //bots chat stripe
+    const uniqeId = generateUniqueId();
+    chatContainer.innerHTML += chatStripe(true, " ", uniqeId);
 
-  //bots chat stripe
-  const uniqeId = generateUniqueId();
-  chatContainer.innerHTML += chatStripe(true, " ", uniqeId);
+    chatContainer.scrollTop = chatContainer.scrollHeight;
 
-  chatContainer.scrollTop = chatContainer.scrollHeight;
+    const messageDiv = document.getElementById(uniqeId);
 
-  const messageDiv = document.getElementById(uniqeId);
+    loader(messageDiv);
 
-  loader(messageDiv);
+    // fetch the data from the server
 
-  // fetch the data from the server
+    const response = await fetch("https://chat-gpt-clone-nhin.onrender.com/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            prompt: data.get("prompt"),
+        }),
+    });
 
-  const response = await fetch("https://chat-gpt-clone-nhin.onrender.com/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      prompt: data.get("prompt"),
-    }),
-  });
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = "";
 
-  clearInterval(loadInterval);
-  messageDiv.innerHTML = "";
+    if (response.ok) {
+        const data = await response.json();
+        const parseData = data.bot.trim();
 
-  if (response.ok) {
-    const data = await response.json();
-    const parseData = data.bot.trim();
+        typetext(messageDiv, parseData);
+    } else {
+        const err = await response.text();
 
-    typetext(messageDiv, parseData);
-  } else {
-    const err = await response.text();
-
-    messageDiv.innerHTML = "Something Went Wrong.";
-  }
+        messageDiv.innerHTML = "Something Went Wrong.";
+    }
 };
 
 form.addEventListener("submit", handleSubmit);
 
 form.addEventListener("keyup", (e) => {
-  if (e.keyCode == 13) {
-    handleSubmit(e);
-  }
+
+    if (e.keyCode == 13) {
+        handleSubmit(e);
+    }
 });
